74df8c4a9e84c4d9cab44552e79f69bb00f53f15
yennguyen <cuongnd.ba12-034@st.usth.edu.vn>
2025-01-08 14:59:56 +0700

    fix bug

--- project_crypto/runcodeinhere/frame.py
+++ project_crypto/runcodeinhere/frame.py
@@ -9,7 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from runcodeinhere.rsa import RSA
+from rsa import RSA
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
